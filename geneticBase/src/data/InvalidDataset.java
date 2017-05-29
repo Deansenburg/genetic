b66ea8b2563a39df1f73d93faa207aab7ca2ca82
Deansenburg <dmurphy1994@hotmail.co.uk>
2017-05-29 15:35:54 +0100

    removal of data helpers as there is no need for it

--- geneticBase/src/data/InvalidDataset.java
+++
@@ -1,9 +0,0 @@
-package data;
-public class InvalidDataset extends Exception
-	{
-		private static final long serialVersionUID = 1L;
-
-		public InvalidDataset(String msg) {
-			super(msg);
-		}
-	}
